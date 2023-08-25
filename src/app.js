ec2e2313cbc54a2012f2298c320141d8c5b5dd07
Mkzay <80535194+Mkzay@users.noreply.github.com>
2023-08-25 10:45:33 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const adviceNo = document.querySelector('#Advice-no');
+const adviceQuote = document.querySelector('#Advice-quote');
+const adviceButton = document.querySelector('#Advice-Button-Generator');
+
+adviceButton.addEventListener('click', () => {
+  fetch('https://api.adviceslip.com/advice')
+    .then(response => response.json())
+    .then(data => {
+      const advice = data.slip.advice;
+      const adviceId = data.slip.id;
+
+      adviceNo.textContent = `${adviceId}`;
+      adviceQuote.textContent = advice;
+    })
+    .catch(error => {
+      console.error('Error fetching advice:', error);
+    });
+});
